a3412d6218d284d7dc5a31012cc8be9dc3fb8a4b
Fajar Muhammad Noor Rozaqi / ザキ <46889787+fajarmnrozaki@users.noreply.github.com>
2022-09-05 16:24:54 +0700

    Uploud new codes of BMS dashboard

---
+++ pages/battery-status.php
@@ -0,0 +1,5 @@
+<div id="battery-status" class="tabcontent" style="display:none;">
+    <h3>battery-status</h3>
+    <p>Sed eu efficitur lorem. Pellentesque varius nulla nec lectus consectetur ullamcorper. In at urna
+        orci. Sed ut malesuada elit, vel tempor leo.</p>
+</div>
---
+++ pages/yearly-report.php
@@ -0,0 +1,5 @@
+<div id="yearly-report" class="tabcontent" style="display:none;">
+    <h3>yearly-report</h3>
+    <p>Fusce arcu magna, mollis eu cursus ut, mollis sit amet quam. Aenean mattis turpis vitae iaculis
+        venenatis. Cras tempus gravida quam in malesuada.</p>
+</div>
