a0098dadc3d2c0889d56f409197e09c32c7ca5ef
RaihanRafif <raihanrafif200095@gmail.com>
2022-10-21 00:21:27 +0700

    make frontend outlines, complete several parts according to design

--- components/newNavbar.php
+++ components/newNavbar.php
@@ -4,8 +4,6 @@
 
 <head>
     <meta charset="UTF-8">
-    <!--<title> Responsive Sidebar Menu  | CodingLab </title>-->
-    <link rel="stylesheet" href="style.css">
     <!-- Boxicons CDN Link -->
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,11 +17,6 @@
             <i class='bx bx-menu' id="btn"></i>
         </div>
         <ul class="nav-list">
-            <!-- <li>
-          <i class='bx bx-search' ></i>
-         <input type="text" placeholder="Search...">
-         <span class="tooltip">Search</span>
-      </li> -->
             <li onclick="openTab(event, 'battery-status')">
                 <a href="#">
                     <i class="fa-solid fa-battery-empty  fa-2xl"></i>
@@ -61,9 +54,7 @@
             </li>
         </ul>
     </div>
-    <section class="home-section">
-        <div class="text">Dashboard</div>
-    </section>
+   
     <script>
         let sidebar = document.querySelector(".sidebar");
         let closeBtn = document.querySelector("#btn");
--- pages/battery-status.php
+++ pages/battery-status.php
@@ -1,5 +1,50 @@
-<div id="battery-status" class="tabcontent" style="display:none;">
-    <h3>battery-status</h3>
-    <p>Sed eu efficitur lorem. Pellentesque varius nulla nec lectus consectetur ullamcorper. In at urna
-        orci. Sed ut malesuada elit, vel tempor leo.</p>
+<div id="battery-status" class="tabcontent" style="display:block;">
+    <div class="title-panel-section">
+        <div class="electrity">
+            <h5>Electricity Status</h5>
+        </div>
+        <div class="kwh">
+            <h5>kWh Electricity Status</h5>
+        </div>
+        <div class="cost">
+            <h5>Cost Electricity</h5>
+        </div>
+        <div class="carbon">
+            <h5>Carbon Footprint</h5>
+        </div>
+        <div class="status">
+            <h5>Data Communication Status</h5>
+        </div>
+    </div>
+    <div class="panel-section">
+        <div class="electrity"></div>
+        <div class="kwh">
+            <ul>
+                <li>Smart Plug 1</li>
+                <li>2</li>
+                <li>Smart Plug 2</li>
+                <li>4</li>
+                <li>Smart Plug 3</li>
+                <li>6</li>
+                <li>Smart Plug 4</li>
+                <li>y</li>
+                <li>Smart Plug 5</li>
+                <li>y</li>
+                <li>Smart Plug 6</li>
+                <li>y</li>
+                <li>Smart Plug 7</li>
+                <li>y</li>
+                <li>Smart Plug 8</li>
+                <li>y</li>
+                <li>Smart Plug 9</li>
+                <li>y</li>
+                <li>Smart Plug 10</li>
+                <li>y</li>
+            </ul>
+        </div>
+        <div class="cost"></div>
+        <div class="carbon"></div>
+        <div class="status"></div>
+    </div>
+    <div class="chart-section"></div>
 </div>
