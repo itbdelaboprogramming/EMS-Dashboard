7b32ab9f57d27eb595e39ab000c55c9401a17e52
RaihanRafif <raihanrafif200095@gmail.com>
2022-10-23 00:05:08 +0700

    add realtime chart, and some option dropdown

--- pages/battery-status.php
+++ pages/battery-status.php
@@ -46,5 +46,57 @@
         <div class="carbon"></div>
         <div class="status"></div>
     </div>
-    <div class="chart-section"></div>
+    <div class="title-chart-section">
+        <div class="battery-options">
+            <!-- <h5>Smartplug</h5> -->
+            <select class="div-toggle" data-target=".my-info-1">
+                <option value="" selected disabled hidden>Smartplug</option>
+                <option value="Batt-1" data-show=".Batt-1">Batt-1</option>
+                <option value="Batt-2" data-show=".Batt-2">Batt-2</option>
+                <option value="Batt-3" data-show=".Batt-3">Batt-3</option>
+                <option value="Batt-4" data-show=".Batt-4">Batt-4</option>
+                <option value="Batt-5" data-show=".Batt-5">Batt-5</option>
+                <option value="Batt-6" data-show=".Batt-6">Batt-6</option>
+                <option value="Batt-7" data-show=".Batt-7">Batt-7</option>
+                <option value="Batt-8" data-show=".Batt-8">Batt-8</option>
+                <option value="Batt-9" data-show=".Batt-9">Batt-9</option>
+                <option value="Batt-10" data-show=".Batt-10">Batt-10</option>
+                <option value="Batt-11" data-show=".Batt-11">Batt-11</option>
+                <option value="Batt-12" data-show=".Batt-12">Batt-12</option>
+            </select>
+        </div>
+        <div class="electricity" style="margin-left:auto;">
+            <!-- <h5>Electricity</h5> -->
+            <select class="div-toggle" data-target=".my-info-1">
+                <option value="" selected disabled hidden>Electricity</option>
+                <option value="voltage" data-show=".Batt-1">Voltage</option>
+                <option value="current" data-show=".Batt-2">Current</option>
+                <option value="power" data-show=".Batt-3">Power</option>
+            </select>
+        </div>
+        <div class="kwh">
+            <h5>kWh</h5>
+            <!-- <select class="div-toggle" data-target=".my-info-1">
+                <option value="" selected disabled hidden>Electricity</option>
+                <option value="voltage" data-show=".Batt-1">Voltage</option>
+                <option value="current" data-show=".Batt-2">Current</option>
+                <option value="power" data-show=".Batt-3">Power</option>
+            </select> -->
+        </div>
+        <div class="cost">
+            <h5>Cost</h5>
+        </div>
+        <div class="carbon">
+            <h5>Carbon </h5>
+        </div>
+    </div>
+    <div class="chart-section">
+        <div class="Batt-1" id="responsecontainer1"></div>
+        <script>
+            var refreshId = setInterval(function() {
+                $('#responsecontainer1').load('./data/ultrasonic.php');
+                // $('#responsecontainer2').load('./data/batt1.php');
+            }, 1000);
+        </script>
+    </div>
 </div>
